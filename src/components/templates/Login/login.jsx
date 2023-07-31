7c1ea476d60d627b4fe4093adbb26aa7d68d7e8f
let1s3n <letisendev@gmail.com>
2023-07-31 18:44:27 -0500

    fix

--- src/components/templates/Login/login.jsx
+++ src/components/templates/Login/login.jsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { MyContext } from '@/MyContext';
 import { loginRequest } from "@/authConfig";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import styles from './login.module.scss'
 const login = () => {
   const { instance } = useMsal();
@@ -14,7 +15,7 @@ const login = () => {
   });
   const { setIsAdmin, setIsLogged } = useContext(MyContext);
   const router = useRouter();
-
+  const [showPassword, setShowPassword] = useState(false);
   const handleChange = (e) => {
     setCredentials({
       ...credentials,
@@ -40,6 +41,7 @@ const login = () => {
         }
       }
     } catch (error) {
+      console.log("ERROR NAPO");
       console.log("Error: ", error);
       setIsAdmin(false);
       setIsLogged(false);
@@ -53,6 +55,10 @@ const login = () => {
     } else if (loginType === "redirect") {
       instance.loginRedirect(loginRequest).catch((e) => { console.error(`loginRedirect failed: ${e}`) })
     };
+  }
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
   }
   return (
     <Container className="g-0">
@@ -70,12 +76,22 @@ const login = () => {
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="password"
-            onChange={handleChange}
-          />
+          <div className="position-relative">
+            <Form.Control
+              name="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="password"
+              onChange={handleChange}
+            />
+            {
+              showPassword
+                ?
+                <AiFillEyeInvisible onClick={handleShowPassword} className="position-absolute" style={{ cursor: "pointer", width: "1rem", top: "50%", right: ".5rem", transform: "translateY(-0.5rem)" }} />
+                :
+                <AiFillEye onClick={handleShowPassword} className="position-absolute" style={{ cursor: "pointer", width: "1rem", top: "50%", right: ".5rem", transform: "translateY(-0.5rem)" }} />
+            }
+
+          </div>
         </Form.Group>
 
         <Button className="mx-auto d-block" variant="primary" type="submit">Login</Button>
