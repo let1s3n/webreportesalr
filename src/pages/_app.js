94544f366ebeab81f19a2987a5946f8bc57cf897
let1s3n <letisendev@gmail.com>
2023-08-02 21:12:41 -0500

    fkx

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }) {
 
   const checkProfile = async () => {
     try {
-      const response = await fetch("/api/profile");
+      const response = await axios.get("/api/profile");
       if (response.status === 200) {
         setIsLogged(true);
 
