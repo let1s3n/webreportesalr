f9f563b5220e4222fe93dfc161fb1b27c65bebec
let1s3n <letisendev@gmail.com>
2023-08-03 01:54:51 -0500

    fix

--- src/pages/api/check.js
+++ src/pages/api/check.js
@@ -1,16 +1,14 @@
 import { verify } from "jsonwebtoken";
 export default function check(req, res) {
-  try {
-    const { myTokenName } = req.cookies;
+  const { myTokenName } = req.cookies;
+  if (myTokenName) {
     const { email, username, roles } = verify(
       myTokenName,
       process.env.NEXT_PUBLIC_SECRET
     );
-
     return res
       .status(200)
       .json({ email: email, username: username, roles: roles });
-  } catch (error) {
-    return res.status(202).json("Unsuccess");
   }
+  return res.status(202).json("Unsuccess");
 }
