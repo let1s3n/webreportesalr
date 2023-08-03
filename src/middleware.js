8dd377687383fb51ee0920fabd31df82695fa812
let1s3n <letisendev@gmail.com>
2023-08-03 15:16:32 -0500

    fix

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export function middleware(request) {
+  const jwt = request.cookies.get("myTokenName");
+  /* const path = request.nextUrl.pathname; */
+  if (jwt === undefined) {
+    console.log("REQ URL: ", request.url);
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/", "/create", "/dashboard"],
+};
--- src/pages/admin/user.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const user = () => {
-  return <div>User Page</div>;
-};
-
-export default user;
