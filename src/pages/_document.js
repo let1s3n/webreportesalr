b311fb54245d9332c18df57f41eaa98f9888e56f
let1s3n <letisendev@gmail.com>
2023-08-03 13:00:31 -0500

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
